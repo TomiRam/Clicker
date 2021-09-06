61f737eb00a48fbbad5f3ead915aaa82b7107265
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2021-09-06 09:45:01 +0300

    Add files via upload

---
+++ Assets/Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public Text ClicksTotalText;
+
+    float TotalClicks;
+
+
+    public void AddClicks()
+    {
+        TotalClicks++;
+        ClicksTotalText.text = TotalClicks.ToString("0");
+    }
+
+}
